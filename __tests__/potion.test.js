6c52bf3648bef5766a9009167b42cd0fe409b6b8
olliedirker <olliedirker@gmail.com>
2022-07-13 16:18:31 -0500

    testing done

---
+++ __tests__/potion.test.js
@@ -0,0 +1,8 @@
+const Potion = require('../lib/potion.js');
+
+test ('creates a health potion object',()=>{
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
