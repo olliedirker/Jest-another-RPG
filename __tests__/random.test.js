c086d6780778093f840dc12b5ff0d158b9c12831
olliedirker <olliedirker@gmail.com>
2022-07-16 13:31:14 -0500

    added player object

--- __tests__/random.test.js
+++
@@ -1,7 +0,0 @@
-
-const randomNumber = require('../lib/random.js');
-
-test('checks if 10 is eaqual to 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
